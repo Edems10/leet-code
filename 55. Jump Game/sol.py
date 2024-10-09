59cf233a87c26ece6e5b1640843f0a0a18ee0955
Adam Mitrenga <edemsikk@gmail.com>
2024-10-09 23:20:53 +0200

    55.

---
+++ 55. Jump Game/sol.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+
+    def canJump(self, nums: List[int]) -> bool:
+        max_reach = 0
+        for i, num in enumerate(nums):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + num)
+        return True
+
+
+if __name__ == "__main__":
+    nums = [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]
+    print(Solution().canJump(nums))
