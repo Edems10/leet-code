99bb19fea8670bb1479e6defdad13d86bbd2328f
Adam Mitrenga <edemsikk@gmail.com>
2024-10-17 14:08:18 +0200

    feat: 380

--- 55. Jump Game/sol.py
+++ 55. Jump Game/sol.py
@@ -4,6 +4,7 @@ from typing import List
 class Solution:
 
     def canJump(self, nums: List[int]) -> bool:
+        
         max_reach = 0
         for i, num in enumerate(nums):
             if i > max_reach:
@@ -11,7 +12,6 @@ class Solution:
             max_reach = max(max_reach, i + num)
         return True
 
-
 if __name__ == "__main__":
-    nums = [5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0]
+    nums = [5,9,3,2,1,0,2,3,3,1,0,0]
     print(Solution().canJump(nums))
