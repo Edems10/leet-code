617615fe6759255cfa43bd7985b543b6764598f6
Edems <edemsikk@gmail.com>
2024-11-01 16:38:09 +0100

    ref: black

--- 125. Valid Palindrome/sol.py
+++ 125. Valid Palindrome/sol.py
@@ -1,18 +1,21 @@
 import re
+
+
 class Solution:
-    def remove_non_alphanumeric(self,s):
-        return re.sub(r'[^a-zA-Z0-9]', '', s)
+    def remove_non_alphanumeric(self, s):
+        return re.sub(r"[^a-zA-Z0-9]", "", s)
+
     def isPalindrome(self, s: str) -> bool:
         cleaned_string = self.remove_non_alphanumeric(s).lower()
         l = 0
         r = len(cleaned_string) - 1
-        while l<r:
+        while l < r:
             if cleaned_string[l] != cleaned_string[r]:
                 return False
             l = l + 1
             r = r - 1
         return True
-        
-        
+
+
 s = Solution()
 print(s.isPalindrome("A man, a plan, a canal: Panama"))
