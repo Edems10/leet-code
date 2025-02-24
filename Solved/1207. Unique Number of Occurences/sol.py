4f562a645c2b0808e46300f0404c5517b4dded0b
Adam Mitrenga <edemsikk@gmail.com>
2025-02-24 15:27:54 +0100

    🦄 refactor: ruff

--- Solved/1207. Unique Number of Occurences/sol.py
+++ Solved/1207. Unique Number of Occurences/sol.py
@@ -4,24 +4,25 @@ from typing import List
 class Solution:
     def __init__(self):
         pass
-    def uniqueOccurrences(self,arr:List[int])->bool:
+
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
         occurences = {}
         for number in arr:
             current_occurence = occurences.get(number)
             if current_occurence:
-                occurences[number] = current_occurence +1
+                occurences[number] = current_occurence + 1
             else:
                 occurences[number] = 1
-        
-        if len(occurences)!= len(set(occurences.values())):
+
+        if len(occurences) != len(set(occurences.values())):
             return False
         return True
-        
-        
+
 
 def main():
     sol = Solution()
-    print(sol.uniqueOccurences([1,2,2,2,1,1,3]))
-    
+    print(sol.uniqueOccurences([1, 2, 2, 2, 1, 1, 3]))
+
+
 if __name__ == "__main__":
     main()
