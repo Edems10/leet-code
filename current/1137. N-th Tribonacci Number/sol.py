34101d42c31bc0a7836a06f431d164c0fcdfe6b3
Adam Mitrenga <edemsikk@gmail.com>
2025-02-27 22:31:16 +0100

    🦄 refactor: format

--- current/1137. N-th Tribonacci Number/sol.py
+++ current/1137. N-th Tribonacci Number/sol.py
@@ -1,22 +1,22 @@
 from collections import deque
+
+
 class Solution:
     def tribonacci(self, n: int) -> int:
-        trib=deque([0,1,1])
-        if n==3:
+        trib = deque([0, 1, 1])
+        if n == 3:
             return 2
-        elif n==2:
+        if n == 1 or n == 2:
             return 1
-        elif n==1:
+        if n == 0:
             return 0
-        elif n==0:
-            return 0
-        
-        for _ in range(3,n):
+
+        for _ in range(3, n):
             trib.append(sum(trib))
             trib.popleft()
-        return(sum(trib))
-    
+        return sum(trib)
+
 
 s = Solution()
-res =s.tribonacci(25)
+res = s.tribonacci(25)
 print(f"{res=}")
