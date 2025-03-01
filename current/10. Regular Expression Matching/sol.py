3ed645147da3f586fd20b1114a88f80957705059
Adam Mitrenga <edemsikk@gmail.com>
2025-03-01 17:29:20 +0100

    ✨ feat: 10 - need to revisit

--- current/10. Regular Expression Matching/sol.py
+++ current/10. Regular Expression Matching/sol.py
@@ -3,32 +3,35 @@ class Solution:
         last_s = 0
         star_iterator = 0
         found = 0
+        iterator_p = 0
         for patern_char_iterator in range(len(p)):
             if p[patern_char_iterator] == "*":
                 star_iterator = patern_char_iterator
-        iterator_p = star_iterator+1
-        pattern_length = len(p)- iterator_p
-        if iterator_p>= len(p):
+                iterator_p = star_iterator + 1
+        pattern_length = len(p) - iterator_p
+        if iterator_p >= len(p):
             return True
-        while(iterator_p<len(p) and last_s<len(s)):
+        while iterator_p < len(p) and last_s < len(s):
+            remaining_length = last_s - len(s)
             if p[iterator_p] == ".":
-                last_s +=1
-                iterator_p +=1
+                last_s += 1
+                iterator_p += 1
                 found += 1
             if s[last_s] == p[iterator_p]:
-                last_s+=1
-                iterator_p+=1
+                last_s += 1
+                iterator_p += 1
                 found += 1
             else:
                 found = 0
-                last_s +=1
+                last_s += 1
             if found == pattern_length:
-                return True
+                if remaining_length == 0:
+                    return True
         return False
-            
 
-sol= Solution()
-s = "ab"
-p = ".*"
-sad=sol.isMatch(s,p)        
-print(sad)    
+
+sol = Solution()
+s = "aa"
+p = "a"
+sad = sol.isMatch(s, p)
+print(sad)
