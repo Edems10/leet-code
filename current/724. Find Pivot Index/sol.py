dde71bbd22f3b0fc0b94600a5fc853f8432c63ae
Adam Mitrenga <edemsikk@gmail.com>
2025-03-03 22:12:14 +0100

    ✨ feat: 724

---
+++ current/724. Find Pivot Index/sol.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        pivot = 0
+        left_part = 0
+        right_part = sum(nums[1:len(nums)])
+        if left_part == right_part:
+            return pivot
+        for pivot in range (1,len(nums)):
+            left_part = left_part + nums[pivot-1]
+            right_part= right_part - nums[pivot]
+            if left_part == right_part:
+                return pivot
+        return -1 
+
+s = Solution()
+s.pivotIndex([1,7,3])
